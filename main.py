08a184a8e4cf1eb7fdbe31de670c358b9b1c2da9
fbhadha <fbhadha@blindword.com>
2025-03-31 22:04:26 -0400

    rotation

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     fps = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p1 = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2 )
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        p1.update(dt)
+        p1.draw(screen)
         pygame.display.flip()
 
         #frame rate
         dt = fps.tick(60) / 1000
-
 if __name__ == "__main__":
     main()
