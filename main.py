d846bb94272a924ced89a57e228113801c6e0838
fbhadha <fbhadha@blindword.com>
2025-04-01 09:32:33 -0400

    asteroids

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import *
 
@@ -6,8 +8,12 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
     p1 = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2 )
+    asteroid_field = AsteroidField()
     fps = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
