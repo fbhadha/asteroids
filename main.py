7ce2a93e29f463f11f51487dcb91e17f95cbe6f3
fbhadha <fbhadha@blindword.com>
2025-03-31 21:18:38 -0400

    fps

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    fps = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1 == 1:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        #frame rate
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
