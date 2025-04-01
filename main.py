4b3b60734c967b25a855ce2e71b2efa07d6f8256
fbhadha <fbhadha@blindword.com>
2025-03-31 20:58:55 -0400

    screen make

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
-
-main()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1 == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
